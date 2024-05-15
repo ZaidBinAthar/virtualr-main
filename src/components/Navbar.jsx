f94274f7c130e54b76d961e48d24c68bbe558d3f
Zaid Athar <zaid.athar.2009@gmail.com>
2024-05-15 19:00:07 +0500

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,79 @@
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+import logo from "../assets/logo.png";
+import { navItems } from "../constants";
+
+const Navbar = () => {
+  const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setMobileDrawerOpen(!mobileDrawerOpen);
+  };
+
+  return (
+    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
+      <div className="container px-4 mx-auto relative lg:text-sm">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center flex-shrink-0">
+            <img className="h-10 w-10 mr-2" src={logo} alt="Logo" />
+            <span className="text-xl tracking-tight">Zaid VirtualR</span>
+          </div>
+          <ul className="hidden lg:flex ml-14 space-x-12">
+            {navItems.map((item, index) => (
+              <li key={index}>
+                <a href={item.href}>{item.label}</a>
+              </li>
+            ))}
+          </ul>
+          <div className="hidden lg:flex justify-center space-x-12 items-center">
+            <a
+              href="https://accounts.google.com/lifecycle/steps/signup/name?continue=https://myaccount.google.com?utm_source%3Daccount-marketing-page%26utm_medium%3Dcreate-account-button&ddm=0&dsh=S302699463:1715781482170744&flowEntry=SignUp&flowName=GlifWebSignIn&TL=ALv_Gf9KvXa9okZdtV0AkZb_FsSAkX2nVB1CVpXdoDteOwURHZ2TYN_wSf5eYAS9"
+              className="py-2 px-3 border rounded-md"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Sign In
+            </a>
+            <a
+              href="https://accounts.google.com/lifecycle/steps/signup/name?continue=https://myaccount.google.com?utm_source%3Daccount-marketing-page%26utm_medium%3Dcreate-account-button&ddm=0&dsh=S302699463:1715781482170744&flowEntry=SignUp&flowName=GlifWebSignIn&TL=ALv_Gf9KvXa9okZdtV0AkZb_FsSAkX2nVB1CVpXdoDteOwURHZ2TYN_wSf5eYAS9"
+              className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Create an account
+            </a>
+          </div>
+          <div className="lg:hidden md:flex flex-col justify-end">
+            <button onClick={toggleNavbar}>
+              {mobileDrawerOpen ? <X /> : <Menu />}
+            </button>
+          </div>
+        </div>
+        {mobileDrawerOpen && (
+          <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+            <ul>
+              {navItems.map((item, index) => (
+                <li key={index} className="py-4">
+                  <a href={item.href}>{item.label}</a>
+                </li>
+              ))}
+            </ul>
+            <div className="flex space-x-6">
+              <a href="#" className="py-2 px-3 border rounded-md">
+                Sign In
+              </a>
+              <a
+                href="#"
+                className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800"
+              >
+                Create an account
+              </a>
+            </div>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
